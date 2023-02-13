e4a31e26fd0d90a438c76870b7e90fd566ecf390
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2023-02-13 15:29:54 +0200

    Modules extracted
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- nxp/imx8qm-mek/default.nix
+++ nxp/imx8qm-mek/default.nix
@@ -3,26 +3,17 @@
 {
   nixpkgs.overlays = [
     (import ./overlay.nix)
-    (import ../common/overlay.nix)
   ];
 
-  nixpkgs.hostPlatform = "aarch64-linux";
+  imports = [
+    ../common/modules.nix
+  ];
 
-  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_imx8;
-  boot.kernelParams = [ "console=ttyLP0,115200n8" ];
-
-  boot.loader.grub = {
-    enable = lib.mkDefault true;
-    extraFiles = {
+  boot.loader.grub.extraFiles = {
       "imx8qm-mek.dtb" = "${pkgs.linux_imx8}/dtbs/freescale/imx8qm-mek.dtb";
-    };
   };
 
-  disabledModules = [ "profiles/all-hardware.nix" ];
-  boot.initrd.includeDefaultModules = lib.mkForce false;
-
   hardware.deviceTree = {
-    enable = true;
     filter = "imx8qm-*.dtb";
     name = "imx8qm-mek.dtb";
   };
